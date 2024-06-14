09dc790895f52e92faa883a389a350b1a1488095
sachin <sachinb0841@gmail.com>
2024-06-14 15:40:44 +0530

    path problem solve

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
+const path = require("path")
 const cookieParser = require("cookie-parser")
 const { userProtected, adminProtected } = require("./middlewares/protected")
 require("dotenv").config({ path: "./.env" })
